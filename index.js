9be615d9b00880d9ec804f0a3af63ce7c5aafa2a
Nandan V <nandanv2702@gmail.com>
2020-08-15 20:57:21 +0530

    completely changed the layout to that of a chat app and god is this good

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // need to make JS for responsive navbar
 
-document.addEventListener('load', function(){
-  document.getElementById('sidebar-content-long').style.display = "none";
-})
+// function for auto resizing textarea taken from Stack Overflow
+function init() {
+  if (window.attachEvent) {
+    var observe = function(element, event, handler) {
+      element.attachEvent('on' + event, handler);
+    };
+  } else {
+    var observe = function(element, event, handler) {
+      element.addEventListener(event, handler, false);
+    };
+  }
+  var text = document.getElementById('text');
+
+  function resize() {
+    text.style.height = 'auto';
+    text.style.height = text.scrollHeight + 'px';
+  }
+  /* 0-timeout to get the already changed text */
+  function delayedResize() {
+    window.setTimeout(resize, 0);
+  }
+  observe(text, 'change', resize);
+  observe(text, 'cut', delayedResize);
+  observe(text, 'paste', delayedResize);
+  observe(text, 'drop', delayedResize);
+  observe(text, 'keydown', delayedResize);
+
+  text.focus();
+  text.select();
+  resize();
+}
