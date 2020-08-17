568f7766627306bfc0880e9a6e92d8a87a87de07
Nandan V <nandanv2702@gmail.com>
2020-08-18 00:46:54 +0530

    working auth with register and login pages

--- app.js
+++ app.js
@@ -1,23 +1,101 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static("public"));
+// Setting up authentication
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/chatDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
-  res.render('index');
+  if (req.isAuthenticated()) {
+    res.render('index');
+  } else {
+    res.redirect('/register');
+  };
 });
+
+app.route('/register')
+  .get(function(req, res) {
+    res.render('register');
+  })
+  .post(function(req, res) {
+    User.register({
+      username: req.body.username
+    }, req.body.password, function(err, user) {
+      if (err) {
+        console.log('error during registration');
+        res.redirect('/register');
+      } else {
+        passport.authenticate('local')(req, res, function() {
+          res.redirect('/');
+        });
+      };
+    });
+  });
+
+app.route('/login')
+  .get(function(req,res){
+    res.render('login');
+  })
+  .post(function(req, res) {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    req.login(user, function(err){
+      if(err){
+        console.log(`error during login: ${err}`);
+        res.redirect('/')
+      } else {
+        passport.authenticate('local')(req, res, function() {
+          res.redirect('/');
+        });
+      }
+    });
+  });
 
 const users = {}
 
